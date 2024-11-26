370f36c814ef91641fc3eb844788ffb417ec1fe4
Yusuff Sodiq <ysodiqakanni@gmail.com>
2024-11-25 21:18:23 -0600

    Add project files.

---
+++ LecturankWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LecturankWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
