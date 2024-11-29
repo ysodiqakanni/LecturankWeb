2b0f92d36d41b31fa8e4e2c8070f83f82de01fae
Yusuff Sodiq <ysodiqakanni@gmail.com>
2024-11-28 23:50:29 -0600

    add api project

---
+++ LecturankApi/Controllers/Admin/SchoolMgtController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LecturankApi.Controllers.Admin
+{
+    [Route("api/admin/schools")]
+    [ApiController]
+    public class SchoolMgtController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetSchools()
+        {
+            return Ok("hello there");
+        }
+    }
+}
